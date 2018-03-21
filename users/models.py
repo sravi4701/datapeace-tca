7a744bc405cbce1201d9cb33111a4229cf2f8ec4
Ravi Shankar <sravi4701@gmail.com>
2018-03-21 22:54:33 +0530

    create user app and model

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class User(models.Model):
+	id 				= models.AutoField(primary_key=True)
+	first_name 		= models.CharField(max_length=50)
+	last_name 		= models.CharField(max_length=50)
+	company_name 	= models.CharField(max_length=100)
+	age 			= models.IntegerField()
+	city 			= models.CharField(max_length=100)
+	state 			= models.CharField(max_length=100)
+	zip_ 			= models.IntegerField()
+	email 			= models.EmailField(max_length=100)
+	web 			= models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.first_name
